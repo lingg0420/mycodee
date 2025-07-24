07443ac66cc58e5e8754b32f097c6cff69723b89
lingg0420 <lingg61020@gmail.com>
2025-07-24 09:26:56 +0800

    0723

---
+++ index.js
@@ -0,0 +1,73 @@
+console.log("HI!!!")
+let number =1;
+const name = "jac"
+number = 141
+let temp = number
+console.log("這是一個數字"+temp)
+
+temp = 20
+console.log("這是一個數字"+temp)
+
+//let a =10
+//let b =20
+//let c = a
+//a = b
+//b = cs
+//console.log(a,b)
+//邏輯運算子
+//let a = 10
+//let b = 20
+// console.log(a == 10)
+// console.log(a != 10)
+// console.log(a > 10 && b == 20)
+// console.log(a > 10 || b == 20)
+// console.log(1+2)
+// console.log(2-1)
+// console.log(2*3)
+// console.log(6/2)
+// console.log(5%2)
+
+// let a = 10
+// if (a == 10){
+//     console.log("a is 10")
+// }else if(a == 15){
+//     console.log("a is 15")
+//     }else {
+//         console.log("a is not equal 10")
+//     }
+
+// for(let i = 0; i <=10; i++){//i++=i+1，i+=2==i+2
+//  if (i<10){
+//     console.log("*")
+// }else (i<=10){
+// console.log("for loop end")
+
+
+// }}
+// let double = function (num1)
+// {
+//     return num1 * 8
+// }
+// console.log(double(3))
+
+// let saymyname = function(name){
+//     console.log("my name is"+name)
+
+// }
+// saymyname(" 112")
+// saymyname("123")
+
+
+let count = 0;
+for(let i = 0;i <101; i++){
+    //console.log(i)
+    count = count + i;
+}
+    console.log(count)
+    
+
+
+
+
+
+
